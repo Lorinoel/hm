408525cd0c5bb1927ab0bedb8f7aa28d80decdf2
Lorinoel <star37fox@gmail.com>
2026-02-17 21:55:04 +0200

    Add files via upload

---
+++ hm5.py
@@ -0,0 +1,13 @@
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
+
+print(Fore.CYAN + "Це блакитний текст")
+print(Back.MAGENTA + "Це текст на фіолетовому фоні")
+print(Style.BRIGHT + Fore.YELLOW + "Це яскравий жовтий текст")
+print("Цей текст буде звичайним, бо ми використали autoreset=True")
+print(Fore.RED + 'Це червоний текст')
+print(Back.GREEN + 'Це текст на зеленому фоні')
+print(Style.DIM + 'Це текст прозоріший')
+print(Style.BRIGHT + 'Це текст насичений')
+print(Style.RESET_ALL + 'Це текст звичайний')
